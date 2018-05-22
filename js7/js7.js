36e90a08755ad315d53654720f337faaffb3d90b
An <987425154@qq.com>
2018-05-22 23:30:56 +0800

    1.0.3

--- js7/js7.js
+++ js7/js7.js
@@ -1,8 +1,8 @@
 window.onload = function () {
     const esw = document.querySelector('#email-sug-wrapper'),
         ei = document.querySelector('#email-input')
-    let elist = ['@163.com', '@gmail.com', '@126.com', '@qq.com', '@263.net'],
-        liindex = 0 //选项li标签的索引，用此来进行键盘事件选中的判定
+    let eList = ['@163.com', '@gmail.com', '@126.com', '@qq.com', '@263.net'],
+        liIndex = 0 //选项li标签的索引，用此来进行键盘事件选中的判定
 
     //输入框失去焦点
     ei.addEventListener('blur', function () {
@@ -19,7 +19,7 @@ window.onload = function () {
     ei.addEventListener('input', function () {
         if (trim(ei.value)) {
             show()
-            addemail()
+            addEmail()
             reset()
         } else {
             hidden()
@@ -32,35 +32,47 @@ window.onload = function () {
         if (li.length != 0 && esw.style.display == 'block') { //判断是否符合进行键盘事件
             switch (e.keyCode) {
                 case 38:
-                    li[liindex].style.backgroundColor = '#fff' //重置上一项的背景
-                    if (liindex <= 0) {
-                        liindex = 4
+                    setColor(li, liIndex, 0) //重置上一项的背景
+                    if (liIndex <= 0) {
+                        liIndex = 4
                     } else {
-                        liindex--
+                        liIndex--
                     }
-                    li[liindex].style.backgroundColor = 'lightskyblue' //设置背景
+                    setColor(li, liIndex, 1) //设置背景
                     break;
                 case 40:
-                    li[liindex].style.backgroundColor = '#fff'
-                    if (liindex >= 4) {
-                        liindex = 0
+                    setColor(li, liIndex, 0)
+                    if (liIndex >= 4) {
+                        liIndex = 0
                     } else {
-                        liindex++
+                        liIndex++
                     }
-                    li[liindex].style.backgroundColor = 'lightskyblue'
+                    setColor(li, liIndex, 1)
                     break;
                 case 13:
-                    ei.value = li[liindex].innerHTML
+                    ei.value = li[liIndex].innerHTML
                     hidden()
                     break;
             }
         }
     })
 
+    //设置背景颜色
+    function setColor(li, liIndex, color) {
+        if (color == 0) {
+            li[liIndex].style.backgroundColor = '#fff'
+        } else if (color == 1) {
+            li[liIndex].style.backgroundColor = 'lightskyblue'
+        } else if (color == 2) {
+            li[liIndex].style.backgroundColor = 'palevioletred'
+        }
+    }
+
     //重置选中状态
     function reset() {
-        liindex = 0
-        document.querySelectorAll('li')[liindex].style.backgroundColor = 'palevioletred'
+        liIndex = 0
+        let li = document.querySelectorAll('li')
+        setColor(li, liIndex, 2)
     }
 
     //隐藏列表
@@ -74,18 +86,18 @@ window.onload = function () {
     }
 
     //增加email后缀
-    function addemail() {
+    function addEmail() {
         esw.innerHTML = '' //清空文本
         let eivalue = trim(ei.value),
             cindex = eivalue.indexOf('@'), //获取@的索引
-            leftvalue = getleft(eivalue, cindex)
-        getright(eivalue, cindex).forEach(function (val, index) {
+            leftvalue = getLeft(eivalue, cindex)
+        getRight(eivalue, cindex).forEach(function (val, index) {
             let li = document.createElement('li'),
                 content = leftvalue + val,
                 txt = document.createTextNode(content)
             li.appendChild(txt)
             esw.appendChild(li)
-            addli(li, content)
+            addLi(li, content)
         })
     }
 
@@ -95,7 +107,7 @@ window.onload = function () {
     }
 
     //获取@前面输入内容
-    function getleft(eivalue, cindex) {
+    function getLeft(eivalue, cindex) {
         if (cindex >= 0) {
             return eivalue.slice(0, cindex)
         } else {
@@ -104,35 +116,31 @@ window.onload = function () {
     }
 
     //匹配@后面的内容
-    function getright(eivalue, cindex) {
+    function getRight(eivalue, cindex) {
         let gremail = []
         if (cindex == -1) { //未输入@即匹配所有
-            gremail = elist
+            gremail = eList
         } else {
             let rightvalue = eivalue.slice(cindex) //获取@后面的值
-            elist.forEach(function (val) {
+            eList.forEach(function (val) {
                 if (val.indexOf(rightvalue) != -1) { //匹配到@后面的值
                     gremail.push(val)
                 }
             })
             if (gremail.length == 0) { //当所有值匹配不到
-                gremail = elist
+                gremail = eList
             }
         }
         return gremail
     }
 
     //li选项添加事件
-    function addli(li, content) {
+    function addLi(li, content) {
         //添加li点击事件（因为blur事件优于click事件，元素先隐藏再判断是否点击，所以要用mousedown）
         li.addEventListener('mousedown', function () {
             ei.value = content
         })
-        li.addEventListener('mouseenter', function () {
-            li.style.backgroundColor = 'lightskyblue'
-        })
         li.addEventListener('mouseleave', function () {
-            li.style.backgroundColor = '#fff'
             ei.focus() //如将获得焦点放在mousedown下，会先触发获得焦点，再执行input的失去焦点
         })
     }
