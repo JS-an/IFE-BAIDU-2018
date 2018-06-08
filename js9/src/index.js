4468364972a497c9c1d7186a50ef40518f731539
An <987425154@qq.com>
2018-06-08 17:31:30 +0800

    1.0.1

--- js9/src/index.js
+++ js9/src/index.js
@@ -83,6 +83,8 @@
             this.cash = opts.cash
             this.seats = opts.seats
             this.staffs = Array.isArray(opts.staff) ? opts.staff : []
+            this.foods = Array.isArray(opts.foods) ? opts.foods : []
+            this.waitCustomer = Array.isArray(opts.waitCustomer) ? opts.waitCustomer : []
         }
         hire(staff) {
             this.staffs.push(staff)
@@ -93,6 +95,12 @@
                 this.staffs[i].staffId === id && (del = i)
             }
             this.staffs.splice(del, 1)
+        }
+        addFood(food) {
+            this.foods.push(food)
+        }
+        addCustomer(customer) {
+            this.waitCustomer.push(customer)
         }
     }
 
@@ -106,14 +114,17 @@
         food() {}
     }
 
-
     //定义服务员
     class waiter extends staff {
         constructor(id, name, pay) {
             super(id, name, pay)
         }
-        recordFood() {}
-        giveFood() {}
+        recordFood(checkedFood) {
+            console.log(this.staffName + '记录了：' + checkedFood.foodName)
+        }
+        giveFood(checkedFood) {
+            console.log(this.staffName + '端着菜肴过来：您好！您的“' + checkedFood.foodName + '”好了，请慢用')
+        }
     }
 
     //定义厨师
@@ -121,14 +132,24 @@
         constructor(id, name, pay) {
             super(id, name, pay)
         }
-        cooking() {}
+        cooking(checkedFood) {
+            console.log('厨师“' + this.staffName + '”双眼一睁，直直盯住眼前食材，大喝一声：起！“' + checkedFood.foodName + '”经过一千种古法炮制，历时七七四十九天，终于完成了！')
+        }
     }
-
 
     //定义顾客
     class customer {
-        chooseFood() {}
-        eatFood() {}
+        constructor(name) {
+            this.customerName = name
+        }
+        chooseFood(restaurant) {
+            let checkedFood = restaurant.foods[Math.floor(Math.random() * restaurant.foods.length)]
+            console.log(this.customerName + '选择了：' + checkedFood.foodName)
+            return checkedFood
+        }
+        eatFood() {
+            console.log(this.customerName + '端起盘子就是干，差点连盘子都吃进去了')
+        }
     }
 
     //定义食物
@@ -140,17 +161,88 @@
         }
     }
 
+    //餐厅用餐流程
+    let restaurantProcedure = (function () {
+        let instance
+        class procedure {
+            constructor() {
+                this.i = 0
+            }
+            go(restaurant) {
+                this.i++;
+                //顾客排队
+                let cw = []
+                restaurant.waitCustomer.forEach(function (el) {
+                    cw.push(el.customerName)
+                })
+                console.log(this.i + '：排队的顾客有：' + cw)
+                //顾客入座
+                let c = restaurant.waitCustomer.shift()
+                console.log(c.customerName + '进去餐厅用餐')
+                restaurant.seats--;
+                console.log('餐厅剩余座位：' + restaurant.seats)
+                //顾客点菜
+                let checkedFood = c.chooseFood(restaurant)
+                //服务员记录菜名
+                let w = restaurant.staffs[0]
+                w.recordFood(checkedFood)
+                //厨师做菜
+                let d = restaurant.staffs[1]
+                d.cooking(checkedFood)
+                //完成后服务员上菜
+                w.giveFood(checkedFood)
+                //顾客吃
+                c.eatFood()
+                //吃完结账
+                restaurant.cash += checkedFood.foodPrice
+                console.log('餐厅收入:' + checkedFood.foodPrice + '！餐厅总账为:' + restaurant.cash)
+                //顾客离开
+                c = null
+                restaurant.seats++;
+                //排队顾客进入
+                if (restaurant.waitCustomer.length !== 0) {
+                    this.go(restaurant)
+                }
+            }
+        }
+        return {
+            useProcedure: function () {
+                if (!instance) {
+                    instance = new procedure()
+                }
+                return instance
+            }
+        }
+
+    }())
+
     //测试
+    //初始化
     let ifeRestaurant = new restaurant({
         cash: 1000000,
-        seats: 20,
-        staff: []
+        seats: 1,
+        staff: [],
+        foods: [],
+        waitCustomer: []
     })
-
-    let newWaiter = new waiter(1, 'a', 1000)
+    let newWaiter = new waiter(1, 'Coco', 3000)
     ifeRestaurant.hire(newWaiter)
-    let newWaiter1 = new waiter(3, 'b', 2000)
-    ifeRestaurant.hire(newWaiter1)
-    ifeRestaurant.fire(1)
-    console.log(ifeRestaurant.staffs)
+    let newCook = new cook(2, 'Tony', 10000)
+    ifeRestaurant.hire(newCook)
+    let newCustomer = new customer('贝爷')
+    ifeRestaurant.addCustomer(newCustomer)
+    let newCustomer1 = new customer('德爷')
+    ifeRestaurant.addCustomer(newCustomer1)
+    let newfood = new food('麻辣烫', 10, 20)
+    ifeRestaurant.addFood(newfood)
+    let newfood1 = new food('鸳鸯火锅', 100, 200)
+    ifeRestaurant.addFood(newfood1)
+    let newfood2 = new food('烤肉', 150, 300)
+    ifeRestaurant.addFood(newfood2)
+
+    //用餐流程
+    restaurantProcedure.useProcedure().go(ifeRestaurant)
+
+    setInterval(function () {}, 5000)
+
 }())
